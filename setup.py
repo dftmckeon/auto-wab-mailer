3d5579aa2f692656adae746cd20f5e6afa6be1c8
dftmckeon <dftmckeon@gmail.com>
2019-11-14 11:12:49 -0500

    Create setup.py w/ dependencies

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Auto-WAB-Mailer',
+      version='1.0',
+      description='Send WAB Emails Daily!',
+      author='Daniel McKeon',
+      author_email='dftm@wharton.upenn.edu',
+      url='https://www.dftmckeon.com/',
+      packages=['praw', 'pyowm', 'smtplib', 'email.mime.multipart', 'email.mime.text', 'time', 'random'],
+     )
